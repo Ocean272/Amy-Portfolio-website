07d67e0ace5da736c319d69510288b67c5ec35f8
ocean272 <ocean272@hotmail.com>
2022-09-09 02:21:51 +0800

    Updates

---
+++ precache-manifest.e6a7398fe4d6f3c8449804dfff814dce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d62be321c9dc2573513",
+    "url": "/Amy-Portfolio-website/static/css/main.4e97050c.chunk.css"
+  },
+  {
+    "revision": "8d62be321c9dc2573513",
+    "url": "/Amy-Portfolio-website/static/js/main.8d62be32.chunk.js"
+  },
+  {
+    "revision": "5ade28e17497eaa8af5a",
+    "url": "/Amy-Portfolio-website/static/js/1.5ade28e1.chunk.js"
+  },
+  {
+    "revision": "33adeed756adc064f7f9",
+    "url": "/Amy-Portfolio-website/static/js/runtime~main.33adeed7.js"
+  },
+  {
+    "revision": "2eadc88e53d37e051a81a1baa970d575",
+    "url": "/Amy-Portfolio-website/static/media/cv.2eadc88e.pdf"
+  },
+  {
+    "revision": "ec2a4fe5fc23357527a145b7b66e1af5",
+    "url": "/Amy-Portfolio-website/static/media/me.ec2a4fe5.jpg"
+  },
+  {
+    "revision": "87786a10bf468c53fd5ac11b593ac862",
+    "url": "/Amy-Portfolio-website/index.html"
+  }
+];
