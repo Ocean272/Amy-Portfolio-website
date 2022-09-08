a11381651a22beef0f856258a41c2a97c6d962f9
ocean272 <ocean272@hotmail.com>
2022-09-09 01:57:53 +0800

    first commit

---
+++ src/components/testimonials/Testimonals.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonals.css'
+
+const testimonals = () => {
+  return (
+    <section id="testimonals">Testimonals</section>
+  )
+}
+
+export default testimonals
