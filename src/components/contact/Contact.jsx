0aae8c4d253aa4816e6c4c5e4cbfd12c97e0ba1e
Ocean272 <ocean272.hotmail.com>
2022-09-26 12:28:59 +0800

    rectify error

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>amylimhooileng@hotmail.com</h5>
+            <a href='mailto:amylimhooileng@hotmail.com' target='-blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+6596456221</h5>
+            <a href='https://wa.me/+6596456221' target='-blank' >Send a message</a>
+          </article>
+        </div>
+        {/* End of Contact Option */}
+        <form action="">
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows="7" placeholder='Your Message' required />
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
