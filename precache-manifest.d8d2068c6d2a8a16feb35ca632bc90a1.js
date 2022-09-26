c4e3b5732ac875da58543499f96cf52745456c13
Ocean272 <ocean272.hotmail.com>
2022-09-26 12:19:01 +0800

    Updates

---
+++ precache-manifest.d8d2068c6d2a8a16feb35ca632bc90a1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "33a94432272f87c1ccd6",
+    "url": "/Amy-Portfolio-website/static/css/main.7205959e.chunk.css"
+  },
+  {
+    "revision": "33a94432272f87c1ccd6",
+    "url": "/Amy-Portfolio-website/static/js/main.33a94432.chunk.js"
+  },
+  {
+    "revision": "261f9e87587a83247d54",
+    "url": "/Amy-Portfolio-website/static/css/1.cd0abae2.chunk.css"
+  },
+  {
+    "revision": "261f9e87587a83247d54",
+    "url": "/Amy-Portfolio-website/static/js/1.261f9e87.chunk.js"
+  },
+  {
+    "revision": "33adeed756adc064f7f9",
+    "url": "/Amy-Portfolio-website/static/js/runtime~main.33adeed7.js"
+  },
+  {
+    "revision": "d66c8dc37259d97af295becf8d4c2ba2",
+    "url": "/Amy-Portfolio-website/static/media/emptyavatar.d66c8dc3.jpg"
+  },
+  {
+    "revision": "2eadc88e53d37e051a81a1baa970d575",
+    "url": "/Amy-Portfolio-website/static/media/cv.2eadc88e.pdf"
+  },
+  {
+    "revision": "0b6b2aa0c54eb7f9a6c428988756280f",
+    "url": "/Amy-Portfolio-website/static/media/headerpic.0b6b2aa0.jpg"
+  },
+  {
+    "revision": "2cf264c5130cca17acb9aab06ff404a9",
+    "url": "/Amy-Portfolio-website/static/media/me2.2cf264c5.jpg"
+  },
+  {
+    "revision": "03ccf9ac5ec89b8f83388e148b5972ef",
+    "url": "/Amy-Portfolio-website/static/media/proj-1.03ccf9ac.jpg"
+  },
+  {
+    "revision": "c42a99bc22881f87d28b7d4e9c6b9be3",
+    "url": "/Amy-Portfolio-website/static/media/proj-2.c42a99bc.jpg"
+  },
+  {
+    "revision": "23f7424219cf4521711099faaca44985",
+    "url": "/Amy-Portfolio-website/static/media/proj3.23f74242.jpg"
+  },
+  {
+    "revision": "5a18f7a213e4551cfa5147030f924502",
+    "url": "/Amy-Portfolio-website/index.html"
+  }
+];
