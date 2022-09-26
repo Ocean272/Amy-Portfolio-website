429c5837fd7b0fdeb7f6fcf4f59fa7d215d1681a
Ocean272 <ocean272.hotmail.com>
2022-09-26 23:57:25 +0800

    rectify footer navi

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './footer.css'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsTwitter} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
 
 const footer = () => {
+  //eslint-disable-next-line
+  const [ activeNav, setActiveNav] = useState('#')
   return (
     <footer>
       <a href='/#' className='footer__logo'>OCEAN272_Works</a>
@@ -13,8 +15,8 @@ const footer = () => {
         <li><a href='/#'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
-        <li><a href='#portfolios'>Portfolios</a></li>
-        <li><a href='#testimonials/'>Testimonials</a></li>
+        <li><a href='#portfolio' >Portfolios</a></li>
+        <li><a href='#testimonials' >Testimonials</a></li>
         <li><a href='#contact'>Contact</a></li>
       </ul>
 
