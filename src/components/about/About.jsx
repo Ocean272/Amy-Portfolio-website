5e33e8b85ea00f045e1d689e370c20eaddeb2dd8
Ocean272 <ocean272.hotmail.com>
2022-09-27 17:35:37 +0800

    prettier

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './about.css'
-import Me2 from '../../assets/me2.jpg'
-import {BiAward} from 'react-icons/bi'
-import {BiDumbbell} from 'react-icons/bi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import Me2 from "../../assets/me2.jpg";
+import { BiAward } from "react-icons/bi";
+import { BiDumbbell } from "react-icons/bi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
@@ -21,32 +21,40 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
-              <BiAward className='about__icon'/>
+              <BiAward className="about__icon" />
               <h5>Experience</h5>
-              <small>6-month Full-time Programme in Software Developer Immersive, accredited by NTU</small>
+              <small>
+                6-month Full-time Programme in Software Developer Immersive,
+                accredited by NTU
+              </small>
             </article>
             <article className="about__card">
-              <BiDumbbell className='about__icon'/>
+              <BiDumbbell className="about__icon" />
               <h5>Strengths</h5>
               <small>Resilience, strong learner</small>
             </article>
             <article className="about__card">
-              <VscFolderLibrary className='about__icon'/>
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>3 completed Projects</small>
             </article>
           </div>
           <p>
-          Software Developer seeking new career path
-          <br/>
-          <br/>I have just completed a 6-month full-time certification in Software Development Immersive accredited by Nanyang Technological University, with the aspiration to embark on my next career in software development.
+            Software Developer seeking new career path
+            <br />
+            <br />I have just completed a 6-month full-time certification in
+            Software Development Immersive accredited by Nanyang Technological
+            University, with the aspiration to embark on my next career in
+            software development.
           </p>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
