6188f297dae9c95d2b8d10fb36264a1b57eb398e
Ocean272 <ocean272.hotmail.com>
2022-09-27 17:35:37 +0800

    prettier

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,25 +1,33 @@
-import React, { useRef } from 'react'
-import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {BsWhatsapp} from 'react-icons/bs'
-import emailjs from 'emailjs-com'
+import React, { useRef } from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_q570ffb', 'template_tj0axr3', form.current, 'Gm8j2-GBG7i_d8Ytc')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_q570ffb",
+        "template_tj0axr3",
+        form.current,
+        "Gm8j2-GBG7i_d8Ytc"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
 
     e.target.reset();
-  }
+  };
 
   return (
     <section id="contact">
@@ -29,28 +37,44 @@ const contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>amylimhooileng@hotmail.com</h5>
-            <a href='mailto:amylimhooileng@hotmail.com' target='-blank'>Send a message</a>
+            <a href="mailto:amylimhooileng@hotmail.com" target="-blank">
+              Send a message
+            </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon' />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+6596456221</h5>
-            <a href='https://wa.me/+6596456221' target='-blank' >Send a message</a>
+            <a href="https://wa.me/+6596456221" target="-blank">
+              Send a message
+            </a>
           </article>
         </div>
         {/* End of Contact Option */}
         <form ref={form} onSubmit={sendEmail}>
-          <input type='text' name='name' placeholder='Your Full Name' required />
-          <input type='email' name='email' placeholder='Your Email' required />
-          <textarea name='message' rows="7" placeholder='Your Message' required />
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default contact
+export default contact;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import Headerpic2 from '../../assets/headerpic2.jpg'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import Headerpic2 from "../../assets/headerpic2.jpg";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -10,17 +10,19 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello, I'm</h5>
         <h1>Amy Lim</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
-        <div className='me'>
-          <img src={Headerpic2} alt='headerpic' />
+        <div className="me">
+          <img src={Headerpic2} alt="headerpic" />
         </div>
 
-        <a href='#contact' className='scroll__down'>Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
